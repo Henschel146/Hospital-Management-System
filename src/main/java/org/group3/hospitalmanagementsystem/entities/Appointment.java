dc18dca8e72f5dd447a291f9027f193a26d4045e
karimberdiDekhkonov <karimberdidekhkonov0921@gmail.com>
2024-01-28 12:05:18 +0100

    added: Appointment Entity, Appointment Repository

---
+++ src/main/java/org/group3/hospitalmanagementsystem/entities/Appointment.java
@@ -0,0 +1,133 @@
+package org.group3.hospitalmanagementsystem.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+//28.01.2024 @karimberdiDekhkonov
+//APPOINTMENT ENTITY CLASS
+@Entity
+public class Appointment {
+    //AUTO ID GENERATING WITH INTEGER
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer appointmentId;
+
+    //START DATE AND TIME OF APPOINTMENT
+    private LocalDateTime startTime;
+
+    //END DATE AND TIME OF APPOINTMENT
+    private LocalDateTime endTime;
+
+    //ASSIGNED ROOM NUMBER FOR THE APPOINTMENT
+    private int roomNumber;
+
+    //EXTRA NOTES FOR THE APPOINTMENT
+    private String notes;
+
+    //CREATED DAY OF THE APPOINTMENT
+    private LocalDate createdDate;
+
+    //MODIFIED DAY OF THE APPOINTMENT
+    private LocalDate modifiedDate;
+
+    //PATIENT ID FOR THE APPOINTMENT
+    //ONE PATIENT CAN CREATE MULTIPLE APPOINTMENT AND EACH APPOINTMENT SIGNED TO ONE PATIENT ONLY.
+    @ManyToOne
+    private Patient patient;
+
+    //DOCTOR ID FOR THE APPOINTMENT
+    //ONE DOCTOR CAN CREATE MULTIPLE APPOINTMENT AND EACH APPOINTMENT SIGNED TO ONE DOCTOR ONLY.
+    @ManyToOne
+    private Doctor doctor;
+
+    //ENCAPSULATION
+
+    public Appointment() {
+    }
+
+    public Appointment(Integer appointmentId, LocalDateTime startTime, LocalDateTime endTime, int roomNumber, String notes, LocalDate createdDate, LocalDate modifiedDate, Patient patient, Doctor doctor) {
+        this.appointmentId = appointmentId;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.roomNumber = roomNumber;
+        this.notes = notes;
+        this.createdDate = createdDate;
+        this.modifiedDate = modifiedDate;
+        this.patient = patient;
+        this.doctor = doctor;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public LocalDate getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDate createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public LocalDate getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(LocalDate modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public void setAppointmentId(Integer appointmentId) {
+        this.appointmentId = appointmentId;
+    }
+
+    public Integer getAppointmentId() {
+        return appointmentId;
+    }
+}
---
+++ src/main/java/org/group3/hospitalmanagementsystem/repository/AppointmentRepository.java
@@ -0,0 +1,9 @@
+package org.group3.hospitalmanagementsystem.repository;
+
+import org.group3.hospitalmanagementsystem.entities.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//28.01.2024 @karimberdiDekhkonov
+//APPOINTMENT REPOSITORY INTERFACE
+public interface AppointmentRepository extends JpaRepository<Appointment, Integer> {
+}
